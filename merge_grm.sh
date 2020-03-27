2bd303e692e783de8ab689d50fd287e717a23cea
Ubuntu <ubuntu@ip-172-31-7-233.us-east-2.compute.internal>
2020-03-27 22:49:12 +0000

    made cmod. updated grm script, added merge grm script

--- merge_grm.sh
+++ merge_grm.sh
@@ -1,4 +1,5 @@
 # Generate a genetic relatedness matrix from .bim, .bed, and .fam files using gcta (available here: https://cnsgenomics.com/software/gcta/#Download)
 
 cd /data/casecontrol_data/
-gcta64 --mbfile test.list --autosome --make-grm --out ubk_cases
+gcta64 --mgrm grm_chrs.list --make-grm --out ukb_cases
+
