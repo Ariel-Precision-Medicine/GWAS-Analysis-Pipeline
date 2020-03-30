aa2fe9e5898ab977ead119c3f9269bdcaabc1a38
dspagnolo09 <ubuntu@ip-172-31-7-233.us-east-2.compute.internal>
2020-03-30 16:26:25 +0000

    script to extract patients from genetic data

---
+++ extract_gwas_patients.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#for CHR in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y}
+for CHR in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y
+
+do
+
+plink --bfile ukb_cal_chr${CHR}_v2 --keep case_control_ids.txt --make-bed --out ukb_chr${CHR}_cases 
+
+done
