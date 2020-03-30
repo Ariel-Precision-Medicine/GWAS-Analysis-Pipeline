882fa64505eb26278c669c6b7b5173b067c4a89d
dspagnolo09 <ubuntu@ip-172-31-7-233.us-east-2.compute.internal>
2020-03-30 18:04:12 +0000

    updated scripts for current analyses

--- make_pca.sh
+++ make_pca.sh
@@ -1,3 +1,3 @@
 # Generate 10 PCAs from a genetic relatedness matrix using gcta (available here: https://cnsgenomics.com/software/gcta/#Download)
-
-gcta64 --grm NAPS2 --pca 10 --out NAPS2
+cd /data/casecontrol_data/
+gcta64 --grm ukb_cases --pca 10 --out ukb_cases
