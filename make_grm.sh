a8894b9f9e080b219db4311508b21ef75c16418c
dspagnolo09 <daniel.m.spagnolo@gmail.com>
2020-04-14 13:54:57 +0000

    modified scripts for matched dataset

--- make_grm.sh
+++ make_grm.sh
@@ -1,4 +1,7 @@
 # Generate a genetic relatedness matrix from .bim, .bed, and .fam files using gcta (available here: https://cnsgenomics.com/software/gcta/#Download)
 
-cd /data/casecontrol_data/
-gcta64 --mbfile test.list --autosome --make-grm --out ukb_cases
+#cd /data/casecontrol_data/
+cd /data/matched_data/
+
+#gcta64 --mbfile test.list --autosome --make-grm --out ukb_cases
+gcta64 --mbfile test.list --autosome --make-grm --out ukb_matched
--- make_pca.sh
+++ make_pca.sh
@@ -1,3 +1,6 @@
 # Generate 10 PCAs from a genetic relatedness matrix using gcta (available here: https://cnsgenomics.com/software/gcta/#Download)
-cd /data/casecontrol_data/
-gcta64 --grm ukb_cases --pca 10 --out ukb_cases
+#cd /data/casecontrol_data/
+cd /data/matched_data/
+
+#gcta64 --grm ukb_cases --pca 10 --out ukb_cases
+gcta64 --grm ukb_matched --pca 10 --out ukb_matched
