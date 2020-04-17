61238662ad65d56c19d406270bda23e9fc433edb
dspagnolo09 <daniel.m.spagnolo@gmail.com>
2020-04-17 19:37:54 +0000

    ran ap only and cp only analyses

--- make_pca.sh
+++ make_pca.sh
@@ -1,6 +1,10 @@
 # Generate 10 PCAs from a genetic relatedness matrix using gcta (available here: https://cnsgenomics.com/software/gcta/#Download)
 #cd /data/casecontrol_data/
-cd /data/matched_data/
+#cd /data/matched_data/
+#cd /data/cp_data/
+cd /data/ap_data/
 
 #gcta64 --grm ukb_cases --pca 10 --out ukb_cases
-gcta64 --grm ukb_matched --pca 10 --out ukb_matched
+#gcta64 --grm ukb_matched --pca 10 --out ukb_matched
+#gcta64 --grm cp_only --pca 10 --out cp_only
+gcta64 --grm ukb_aponly --pca 10 --out ukb_aponly
--- remove_quotes.sh
+++ remove_quotes.sh
@@ -1,1 +1,1 @@
-cat pancreatitis_vs_control_discrete_covars_matched.txt | sed 's/"//g' > pancreatitis_vs_control_discrete_covars_matched.txt 
+cat pancreatitis_vs_control_discrete_covars_matched.txt | sed 's/"//g' > pancreatitis_vs_control_discrete_covars_matched_nq.txt 
